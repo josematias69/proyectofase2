737b01cd47a6e49eadbfa04dd844766e95caa27a
josematias69 <jejebdiejoskf@gmail.com>
2026-03-20 20:17:18 -0600

    Mejoras en las excepciones de error fase 2

--- ScriptException.java
+++ ScriptException.java
@@ -1,6 +1,20 @@
 public class ScriptException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
+
+    public ScriptException() {
+        super("Error en la ejecución del Script.");
+    }
+
     public ScriptException(String message) {
         super(message);
+    }
+
+    public ScriptException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ScriptException(Throwable cause) {
+        super(cause);
     }
 }
