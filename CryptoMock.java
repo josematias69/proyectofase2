e7232cad921184af5c69cfe6afac53c48e43e8d8
josematias69 <jejebdiejoskf@gmail.com>
2026-03-20 20:34:31 -0600

    CrytoMock

--- CryptoMock.java
+++ CryptoMock.java
@@ -1,10 +1,10 @@
 public class CryptoMock {
 
     public static String hash160(String input) {
-        return "HASH_" + input;
+        return "HASH_" + input.toUpperCase();
     }
 
     public static boolean checkSig(String signature, String pubKey) {
-        return signature.equals("SIG_OK") && pubKey.equals("PUBKEY_OK");
+        return signature.startsWith("SIG_") && pubKey.startsWith("PUBKEY_");
     }
 }
